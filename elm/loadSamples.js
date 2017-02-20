63492c1e2bd645d40d6aa93de6c1477c0eb6d6d5
Richard Feldman <richard.t.feldman@gmail.com>
2017-02-19 19:38:08 -0800

    :skull: dead code

--- elm/loadSamples.js
+++ elm/loadSamples.js
@@ -85,22 +85,3 @@ function loadSamples() {
   setState(state);
   setTimeout(loadSamples, ENV.timeout);
 }
-
-var _base;
-
-(_base = String.prototype).lpad || (_base.lpad = function(padding, toLength) {
-  return padding.repeat((toLength - this.length) / padding.length).concat(this);
-});
-
-
-function formatElapsed(value) {
-  str = parseFloat(value).toFixed(2);
-  if (value > 60) {
-    minutes = Math.floor(value / 60);
-    comps = (value % 60).toFixed(2).split('.');
-    seconds = comps[0].lpad('0', 2);
-    ms = comps[1];
-    str = minutes + ":" + seconds + "." + ms;
-  }
-  return str;
-}
